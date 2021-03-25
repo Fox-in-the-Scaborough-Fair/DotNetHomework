37071783fbc476e89bb063aac7d379dfd85d4e38
Fox-in-the-Scaborough-Fair <450881581@qq.com>
2021-03-25 16:33:22 +0800

    软件构造基础第四周作业

---
+++ Homework4/Homework4_1/Program.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+
+namespace Homework4_1
+{
+    class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        public class GenericList<T> 
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head
+            {
+                get => head;
+            }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else 
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+                Node<T> point = this.head;      //指向访问结点
+
+                while (point != null)   //当前非空
+                {
+                    action(point.Data);
+                    point = point.Next;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            GenericList<int> myNode = new GenericList<int>();
+            for (int x = 0; x < 10; x++)    //构造链表0->1->2->3->4->5->6->7->8->9
+            {
+                myNode.Add(x);
+            }
+
+            //打印
+            Console.WriteLine("链表元素依次为:");
+            myNode.ForEach(m => Console.Write($"{m}  "));
+            Console.WriteLine();
+            //求最大值
+            int max = -2147483648;      //初始化为最小值
+            myNode.ForEach(m => max = max > m ? max : m);
+            Console.WriteLine($"Max={max}");
+
+            //求最小值
+            int min = 2147483647;      //初始化为最小值
+            myNode.ForEach(m => min = min < m ? min : m);
+            Console.WriteLine($"Min={min}");
+
+            //求和
+            int sum = 0;
+            myNode.ForEach(m => sum += m) ;
+            Console.WriteLine($"Sum={sum}");
+        }
+    }
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+
+namespace Timer
+{
+
+
+    //声明委托类型
+    public delegate void ClockWork(object sender, ClockArgs args); 
+
+    //声明参数类型
+    public class ClockArgs 
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    //定义事件源
+    public class ClockTikTok        
+    {
+        //声明事件
+        public event ClockWork Tick;
+        public event ClockWork Alarm;
+        private int hour,minute,second;
+        public ClockTikTok(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public void TikTok()
+        {
+            this.second++;   //时间前进1s
+            if (this.second >= 60)
+            {
+                this.second = this.second % 60;
+                this.minute++;
+                if (this.minute >= 60)
+                {
+                    this.minute = this.minute % 60;
+                    this.hour++;
+                    if (this.hour >= 24)
+                    {
+                        this.hour = this.hour % 24;
+                    }
+                }
+            }
+            ClockArgs args = new ClockArgs()
+            {
+                Hour = this.hour, Minute = this.minute, Second = this.second
+            };
+            Tick(this, args);
+            Alarm(this, args);
+        }
+    }
+
+    public class UseClock
+    {
+        static void Main(string[] args)
+        {
+            uint batteryCapacity = 14630400;        //模拟闹钟可运行十天(24*60*60秒)
+
+            ClockTikTok myClock = new ClockTikTok(23, 59, 50);  //初始化闹钟
+
+            //注册事件
+            myClock.Tick += ShowTick;
+            myClock.Alarm += ShowAlarm;
+
+            //事件处理方法
+            static void ShowTick(object sender, ClockArgs now)
+            {
+                Console.WriteLine($"TikTok! Time is {now.Hour}:{now.Minute}:{now.Second}");
+            }
+
+            static void ShowAlarm(object sender, ClockArgs now) //设置每过一分钟闹钟触发一次
+            {
+                if (now.Second == 0)
+                {
+                    Console.WriteLine($"Alarm! DingRing! DingRing! DingRing! DingRing! DingRing!");
+                }
+            }
+            while (batteryCapacity > 0)
+            {
+                System.Threading.Thread.Sleep(1000);
+                myClock.TikTok();
+                batteryCapacity--;
+            }
+        }
+    }
+}
