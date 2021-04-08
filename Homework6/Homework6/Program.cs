fe72dff7bee0b5d8415efd8ac135c2ee8b50f837
Fox-in-the-Scaborough-Fair <450881581@qq.com>
2021-04-08 20:17:34 +0800

    软件构造基础第六周作业

---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Homework6
+{
+    //订单明细类
+    public class OrderDetails
+    {
+        public string orderName { get; }       //商品名称
+        public double orderPrice { get; }      //商品价格
+
+        public int orderNum { get; }        //商品购买数量
+        public OrderDetails(string orderName, double orderPrice,int orderNum)
+        {
+                this.orderName = orderName;
+                this.orderPrice = orderPrice;
+                this.orderNum = orderNum;
+        }
+        public OrderDetails()
+        {
+            this.orderName = "";
+            this.orderPrice = 0;
+            this.orderNum = 0;
+        }
+
+        public override string ToString()
+        {
+            return "商品名称:" + orderName + "  商品价格:" + orderPrice + "  商品数量:" + orderNum + "\n";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is OrderDetails details &&
+                   orderName == details.orderName &&
+                   orderPrice == details.orderPrice &&
+                   orderNum == details.orderNum;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(orderName, orderPrice, orderNum);
+        }
+    }
+
+    [Serializable]
+    public class Order
+    {
+        public string client { get; set; }       //订单客户
+        public string orderNo { get; set; }         //订单号
+        public double totalPrice { get; set; }       //总金额
+
+        public List <OrderDetails> orderDetailsList;        //订单明细
+
+
+        public Order(string client, string orderNo, OrderDetails[] allOrderDetails)
+        {
+            this.client = client;
+            this.orderNo = orderNo;
+            this.orderDetailsList = new List<OrderDetails>();
+            foreach (OrderDetails anOrderDetail in allOrderDetails)
+            {
+                this.totalPrice += anOrderDetail.orderPrice * anOrderDetail.orderNum;
+                this.orderDetailsList.Add(anOrderDetail);
+            }
+
+        }
+
+        public Order()
+        {
+            this.client = "";
+            this.orderNo = "";
+            this.orderDetailsList = new List<OrderDetails>();
+        }
+
+
+        public override string ToString()
+        {
+            string temp = "";
+            foreach (OrderDetails anOrderDetail in orderDetailsList)
+            {
+                temp += anOrderDetail.ToString();
+            }
+            return "订单客户:" + client + "  订单号:" + orderNo + "  总金额:" + totalPrice + "\n" + temp;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Order order &&
+                   client == order.client &&
+                   orderNo == order.orderNo &&
+                   totalPrice == order.totalPrice &&
+                   EqualityComparer<List<OrderDetails>>.Default.Equals(orderDetailsList, order.orderDetailsList);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(client, orderNo, totalPrice, orderDetailsList);
+        }
+    }
+
+    [Serializable]
+    public class OrderService
+    {
+        public List<Order> orderList;
+        public OrderService(Order[] allOrders)
+        {
+                this.orderList = new List<Order>();
+                foreach (Order anOrder in allOrders)
+                {
+                    orderList.Add(anOrder);
+                }
+            
+        }
+
+        public OrderService()
+        {
+            this.orderList = new List<Order>();
+        }
+
+        //添加订单
+        public void AddOrder(Order myOrder)
+        {
+            try
+            {
+                this.orderList.Add(myOrder);
+            }
+            catch (System.NullReferenceException)
+            {
+                Console.WriteLine("订单无效!");
+            }
+        }
+
+        //删除订单,通过订单号删除订单
+        public void DeleteOrder(string orderNo)
+        {
+            int i = 0;
+            try
+            {
+                while (i < orderList.Count)
+                {
+                    if (orderList[i].orderNo == orderNo)
+                    {
+                        orderList.RemoveAt(i+1);
+                        break;
+                    }
+                    i++;
+                }
+            }
+            catch (System.IndexOutOfRangeException)     //越界
+            {
+                Console.WriteLine("订单号无效");
+            }
+        }
+
+        //根据旧订单号修改订单信息
+        public void ModifyOrder(string oldOrderNo, string client, string orderNo, OrderDetails[] allOrderDetails)
+        {
+            int i = 0;
+            try
+            {
+                while (i < orderList.Count)
+                {
+                    if (orderList[i].orderNo == oldOrderNo)
+                    {
+                        orderList[i].client = client;
+                        orderList[i].orderNo = orderNo;
+                        orderList[i].orderDetailsList.Clear();
+                        foreach (OrderDetails anOrder in allOrderDetails)
+                        {
+                            orderList[i].totalPrice += anOrder.orderPrice * anOrder.orderNum;
+                            orderList[i].orderDetailsList.Add(anOrder);
+                        }
+                        break;
+                    }
+                    i++;
+                }
+            }
+            catch (System.IndexOutOfRangeException)     //越界
+            {
+                Console.WriteLine("订单号无效");
+            }
+        }
+
+        //通过订单号查询订单
+        public List<Order> FindOrderNo(string orderNo)
+        {
+            var query = from oneOrder in orderList
+                        where oneOrder.orderNo == orderNo
+                        select oneOrder;
+            if (query.Count() > 0)
+            {
+                return query.ToList<Order>();
+            }
+            else
+                return null;
+        }
+
+        //通过商品名称查询订单
+        public List<Order> FindOrderName(string orderName)
+        {
+            var query = from oneOrder in orderList
+                        from oneOrderDetails in oneOrder.orderDetailsList
+                        where oneOrderDetails.orderName == orderName
+                        orderby oneOrder.totalPrice
+                        select oneOrder;
+            if (query.Count() > 0)
+            {
+                return query.ToList<Order>();
+            }
+            else
+                return null;
+        }
+
+        //通过客户查询订单
+        public List<Order> FindClient(string client)
+        {
+            var query = from oneOrder in orderList
+                        where oneOrder.client == client
+                        orderby oneOrder.totalPrice
+                        select oneOrder;
+            if (query.Count() > 0)
+            {
+                return query.ToList<Order>();
+            }
+            else
+                return null;
+        }
+
+        //通过金额查询订单
+        public List<Order> FindTotalPrice(double totalPrice)
+        {
+            var query = from oneOrder in orderList
+                        where oneOrder.totalPrice == totalPrice
+                        orderby oneOrder.totalPrice
+                        select oneOrder;
+            if (query.Count() > 0)
+            {
+                return query.ToList<Order>();
+            }
+            else
+                return null;
+        }
+
+        public void OrderSort()
+        {
+            this.orderList.Sort((p1, p2) => int.Parse(p1.orderNo) - int.Parse(p2.orderNo));
+        }
+
+        //订单序列化
+        public void Export(string exportName)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(exportName, FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, orderList);
+            }
+        }
+
+        //载入订单
+        public void Import(string importName)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(importName, FileMode.Open))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+                    orderList = ((List<Order>)xmlSerializer.Deserialize(fs));
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("找不到该文件！");
+            }
+        }
+
+        public bool ListEquals(OrderService service)
+        {
+            int i;
+            if (this.orderList.Count == 0 && service.orderList.Count == 0)
+            {
+                return true;
+            }
+
+            if (this.orderList.Count == service.orderList.Count)
+            {
+                for (i = 0; i < this.orderList.Count; i++)
+                {
+                    {
+                        if (this.orderList[i] == service.orderList[i])
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        
+
+        public override bool Equals(object obj)
+        {
+
+            return obj is OrderService service && ListEquals(service);
+
+            
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(orderList);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] myOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+
+            OrderService myOrderService = new OrderService(myOrder);
+
+        }
+
+    }
+
+}
+
---
+++ Homework6/Homework6Tests1/OrderServiceTests.cs
@@ -0,0 +1,429 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homework6;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Homework6.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+
+        [TestMethod()]
+        public void AddOrderTest1()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+            Order newOrder = new Order("小徐", "2019302110068", myOrderDetails);
+            testOrderService.AddOrder(newOrder);
+
+            Order[] myOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+            OrderService myOrderService = new OrderService(myOrder);
+            Assert.IsTrue(myOrderService.Equals(testOrderService));
+            
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void AddOrderTest2()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+            testOrderService.AddOrder(null);
+
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest1()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] myOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+            };
+
+            OrderService myOrderService = new OrderService(myOrder);
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+            OrderService testOrderService = new OrderService(testOrder);
+            testOrderService.DeleteOrder("2019302110068");
+
+            CollectionAssert.AreEqual(testOrderService.orderList, myOrderService.orderList);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void DeleteOrderTest2()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+            OrderService testOrderService = new OrderService(testOrder);
+            testOrderService.DeleteOrder("2019302110099");
+        }
+
+        [TestMethod()]
+        public void ModifyOrderTest1()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] myOrder =
+            {
+                new Order("刘雨辛", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+
+            OrderService myOrderService = new OrderService(myOrder);
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+            OrderService testOrderService = new OrderService(testOrder);
+            testOrderService.ModifyOrder("2019302110066","刘雨辛","2019302110066",myOrderDetails);
+            CollectionAssert.AreEqual(testOrderService.orderList, myOrderService.orderList);
+            
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ModifyOrderTest2()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myOrderDetails)
+            };
+            OrderService testOrderService = new OrderService(testOrder);
+            testOrderService.ModifyOrder("2019302110099", "刘雨辛", "2019302110066", myOrderDetails);
+
+        }
+
+
+        [TestMethod()]
+        public void FindOrderNoTest()
+        {
+            OrderDetails[] myOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+
+            OrderDetails[] myAnotherOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+                new OrderDetails("橡皮",2.0,1)
+            };
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myAnotherOrderDetails)
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+
+            List<Order> testOrders = testOrderService.FindOrderNo("2019302110066");
+            List<Order> myOrders = new List<Order>();
+            myOrders.Add(new Order("小刘", "2019302110066", myOrderDetails));
+            CollectionAssert.AreEqual(testOrders, myOrders);
+        }
+
+        [TestMethod()]
+        public void FindOrderNameTest()
+        {
+            OrderDetails[] myOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+
+            OrderDetails[] myAnotherOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+                new OrderDetails("橡皮",2.0,1)
+            };
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myAnotherOrderDetails)
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+
+            List<Order> testOrders = testOrderService.FindOrderName("橡皮");
+            List<Order> myOrders = new List<Order>();
+            myOrders.Add(new Order("小徐", "2019302110068", myAnotherOrderDetails));
+            CollectionAssert.AreEqual(testOrders, myOrders);
+        }
+
+        [TestMethod()]
+        public void FindClientTest()
+        {
+            OrderDetails[] myOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+
+            OrderDetails[] myAnotherOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+                new OrderDetails("橡皮",2,1)
+            };
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myAnotherOrderDetails)
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+
+            List<Order> testOrders = testOrderService.FindClient("小詹");
+            List<Order> myOrders = new List<Order>();
+            myOrders.Add(new Order("小詹", "2019302110067", myOrderDetails));
+            CollectionAssert.AreEqual(testOrders, myOrders);
+        }
+
+        [TestMethod()]
+        public void FindTotalPriceTest()
+        {
+            OrderDetails[] myOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+
+            OrderDetails[] myAnotherOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+                new OrderDetails("橡皮",2,1)
+            };
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myAnotherOrderDetails)
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+
+            List<Order> testOrders = testOrderService.FindTotalPrice(72.5);
+            List<Order> myOrders = new List<Order>();
+            myOrders.Add(new Order("小刘", "2019302110066", myOrderDetails));
+            myOrders.Add(new Order("小詹", "2019302110067", myOrderDetails));
+            myOrders.Add(new Order("小龙", "2019302110063", myOrderDetails));
+            CollectionAssert.AreEqual(testOrders, myOrders);
+        }
+
+        [TestMethod()]
+        public void OrderSortTest()
+        {
+            OrderDetails[] myOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+
+            OrderDetails[] myAnotherOrderDetails =
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+                new OrderDetails("橡皮",2,1)
+            };
+
+            Order[] testOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小徐", "2019302110068",myAnotherOrderDetails)
+            };
+
+
+            Order[] myOrder =
+            {
+                new Order("小龙", "2019302110063",myOrderDetails),
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小徐", "2019302110068",myAnotherOrderDetails)
+            };
+
+            OrderService testOrderService = new OrderService(testOrder);
+            OrderService myOrderService = new OrderService(myOrder);
+
+            testOrderService.OrderSort();
+
+            CollectionAssert.AreEqual(testOrderService.orderList, myOrderService.orderList);
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] myOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+            };
+
+            OrderService myOrderService = new OrderService(myOrder);
+
+            myOrderService.Export("test.xml");
+
+            FileStream testReadStream = new FileStream("test.xml",FileMode.Open, FileAccess.Read);
+            FileStream myReadStream = new FileStream("orderExport.xml", FileMode.Open, FileAccess.Read);
+
+            string testResult = "";
+            string myResult = "";
+            for (int a = testReadStream.ReadByte(); a != -1; a = testReadStream.ReadByte())
+            {
+                testResult += a;
+            }
+
+            for (int b = myReadStream.ReadByte(); b != -1; b = myReadStream.ReadByte())
+            {
+                myResult += b;
+            }
+
+            StringAssert.Equals(myResult, testResult);
+
+        }
+
+        [TestMethod()]
+        public void ImportTest1()
+        {
+            OrderService testOrderService = new OrderService();
+            testOrderService.Import("orderExport.xml");
+
+            OrderDetails[] myOrderDetails = new OrderDetails[3]
+            {
+                new OrderDetails("笔记本", 4.0, 10),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+            };
+            Order[] myOrder =
+            {
+                new Order("小刘", "2019302110066",myOrderDetails),
+                new Order("小詹", "2019302110067",myOrderDetails),
+                new Order("小龙", "2019302110063",myOrderDetails),
+            };
+
+            OrderService myOrderService = new OrderService(myOrder);
+
+            CollectionAssert.AreEqual(myOrderService.orderList, testOrderService.orderList);
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ImportTest2()
+        {
+            OrderService testOrderService = new OrderService();
+            testOrderService.Import("anError.xml");
+        }
+    }
+}
