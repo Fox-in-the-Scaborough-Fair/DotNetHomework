6046752eb8d8dc629179cdd36880d60519bfb7f8
Fox-in-the-Scaborough-Fair <450881581@qq.com>
2021-04-23 00:49:17 +0800

    软件构造基础第八周作业

---
+++ Homework8/Homework8/Form1.cs
@@ -0,0 +1,216 @@
+﻿using Homework6;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Homework8
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Action<string> SendMsg { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.DataSource = myOrderService.orderList;
+
+            dataGridView1.TopLeftHeaderCell.Value = "序号";
+            dataGridView3.TopLeftHeaderCell.Value = "序号";
+
+        }
+
+        //OrderService myOrderService = new OrderService();
+
+        static OrderDetails[] myOrderDetails1 = new OrderDetails[4]
+            {
+                new OrderDetails("笔记本", 4.0, 1),
+                new OrderDetails("签字笔", 5.0 , 5),
+                new OrderDetails("计算器", 7.5, 1),
+                new OrderDetails("圆规", 10.0, 1),
+            };
+
+        static OrderDetails[] myOrderDetails2 = new OrderDetails[3]
+        {
+            new OrderDetails("笔记本", 4.0, 2),
+            new OrderDetails("签字笔", 5.0 , 5),
+            new OrderDetails("计算器", 7.5, 1),
+        };
+        static OrderDetails[] myOrderDetails3 = new OrderDetails[2]
+        {
+            new OrderDetails("橡皮",1.5, 1),
+            new OrderDetails("铅笔", 1.0 , 5),
+        };
+        static OrderDetails[] myOrderDetails4 = new OrderDetails[1]
+        {
+                new OrderDetails("笔记本", 4.0, 4),
+        };
+
+        static Order[] myOrder =
+        {
+                new Order("小刘", "2019302110066",myOrderDetails1),
+                new Order("小詹", "2019302110067",myOrderDetails2),
+                new Order("小龙", "2019302110063",myOrderDetails3),
+                new Order("小徐", "2019302110068",myOrderDetails4)
+            };
+
+        public OrderService myOrderService = new OrderService(myOrder);
+
+        Form2 createAndModify = new Form2();
+
+        public int No;
+
+        public event EventHandler SendMsgEvent;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "新建订单";
+            Form2 createAndModify = new Form2();
+            createAndModify.Text = "新建订单";
+            createAndModify.ShowDialog(this);
+            createAndModify.createOrModify = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "删除成功！";
+            myOrderService.DeleteByNum(No);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = myOrderService.orderList;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "修改订单";
+            Form2 createAndModify = new Form2();
+            createAndModify.fatherForm = this;
+            createAndModify.Text = "修改订单";
+            createAndModify.Show(this);
+            createAndModify.createOrModify = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "查询订单";
+            List<Order> ordersFound;
+            if (textBox1.Text == "")
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = myOrderService.orderList;
+            }
+            else
+            {
+                ordersFound = myOrderService.FindOrderNo(textBox1.Text);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = ordersFound;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "导出订单";
+
+            SaveFileDialog newExportXml = new SaveFileDialog();
+            newExportXml.ShowDialog();
+            if (myOrderService.Export(newExportXml.FileName))
+            {
+                MessageBox.Show("导出成功", "导出提示");
+            }
+            else
+            {
+                MessageBox.Show("导出失败", "导入提示");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "导入订单";
+            OpenFileDialog newImportXml = new OpenFileDialog();
+            newImportXml.ShowDialog();
+
+            if (myOrderService.Import(newImportXml.FileName))
+            {
+                MessageBox.Show("导入成功", "导入提示");
+            }
+            else 
+            {
+                MessageBox.Show("导入失败", "导入提示");
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = myOrderService.orderList;
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+                                                e.RowBounds.Location.Y,
+                                                dataGridView1.RowHeadersWidth - 4,
+                                                e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics,
+                                 (e.RowIndex + 1).ToString(),
+                                 dataGridView1.RowHeadersDefaultCellStyle.Font,
+                                 rectangle,
+                                 dataGridView1.RowHeadersDefaultCellStyle.ForeColor,
+                                 TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+
+        private void dataGridView3_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+                                    e.RowBounds.Location.Y,
+                                    dataGridView3.RowHeadersWidth - 4,
+                                    e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics,
+                                 (e.RowIndex + 1).ToString(),
+                                 dataGridView3.RowHeadersDefaultCellStyle.Font,
+                                 rectangle,
+                                 dataGridView3.RowHeadersDefaultCellStyle.ForeColor,
+                                 TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView3.DataSource = myOrderService.orderList[e.RowIndex].orderDetailsList;
+            No = e.RowIndex;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+
+}
---
+++ Homework8/Homework8/Form2.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Homework6;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework8
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            if (createOrModify)
+            {
+                this.Text = "新建订单";
+
+            }
+            else 
+            {
+                this.Text = "修改订单";
+            }
+
+            dataGridView2.DataSource = temp.orderDetailsList;
+        }
+
+
+    public int No;
+
+        public bool createOrModify;
+
+        Order temp = new Order();
+        Order tempOrder = new Order();
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1 fatherForm;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tempOrder.orderNo = textBox1.Text;
+            tempOrder.client = textBox2.Text;
+            if (createOrModify == true)
+            {
+                
+            }
+           
+        }
+
+        //输入订单号
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //输入客户
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //添加明细
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OrderDetails newDetails = new OrderDetails(textBox3.Text, Convert.ToDouble(textBox4.Text), int.Parse(textBox5.Text));
+                tempOrder.orderDetailsList.Add(newDetails);
+            }
+            catch (System.FormatException)
+            {
+                
+            }
+
+            dataGridView2.DataSource = temp.orderDetailsList;
+            dataGridView2.DataSource = tempOrder.orderDetailsList;
+
+        }
+
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            No = e.RowIndex;
+        }
+
+        private void dataGridView2_RowPostPaint_1(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+                        e.RowBounds.Location.Y,
+                        dataGridView2.RowHeadersWidth - 4,
+                        e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics,
+                                 (e.RowIndex + 1).ToString(),
+                                 dataGridView2.RowHeadersDefaultCellStyle.Font,
+                                 rectangle,
+                                 dataGridView2.RowHeadersDefaultCellStyle.ForeColor,
+                                 TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+        
+        //输入商品名称
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //输入商品价格
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //输入商品数量
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //修改明细
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tempOrder.orderDetailsList[No].orderName = textBox3.Text;
+                tempOrder.orderDetailsList[No].orderPrice = Convert.ToDouble(textBox4.Text);
+                tempOrder.orderDetailsList[No].orderNum = int.Parse(textBox5.Text);
+                dataGridView2.DataSource = temp.orderDetailsList;
+                dataGridView2.DataSource = tempOrder.orderDetailsList;
+            }
+            catch (System.FormatException)
+            {
+
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+
+            }
+        }
+
+        //删除明细
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tempOrder.orderDetailsList.RemoveAt(No);
+                dataGridView2.DataSource = temp.orderDetailsList;
+                dataGridView2.DataSource = tempOrder.orderDetailsList;
+            }
+            catch (System.FormatException)
+            {
+
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+
+            }
+        }
+
+
+    }
+}
