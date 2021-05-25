51de4b5b59b230ba41b4e74e3a6e1a423ad3e4e0
Fox-in-the-Scaborough-Fair <450881581@qq.com>
2021-05-25 20:57:06 +0800

    软件构造基础第九周作业

---
+++ Homework9/Homework9/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using SimpleCrawler;
+
+namespace Homework9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //绑定数据
+            UrlGet.DataSource = myCrawler.UrlGetList;
+            UrlLose.DataSource = myCrawler.UrlLostList;
+        }
+
+        MyCrawler myCrawler = new MyCrawler();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myCrawler.urls.Add(UrlInput.Text, false);
+                new Thread(myCrawler.Crawl).Start();
+            }
+            catch (System.ArgumentException)
+            {
+                Warning.Text = "请勿重复添加相同网址!";
+            }
+
+            UrlGet.ResetBindings(false);
+            UrlLose.ResetBindings(false);
+        }
+
+        private void UrlInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UrlGetView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Homework9/SimpleCrawler/SimpleCrawler/Program.cs
@@ -0,0 +1,256 @@
+﻿/*
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler
+{
+    public class MyCrawler
+    {
+        public Hashtable urls = new Hashtable();
+        public int count = 0;
+        public List<Url> UrlGetList = new List<Url>();
+        public List<Url> UrlLostList = new List<Url>();
+
+        public void Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = this.DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                this.Parse(html, current);//解析,并加入新的链接
+                Console.WriteLine("爬行结束");
+            }
+        }
+
+        private string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                //添加URL爬取错误的信息
+                UrlLostList.Add(new Url(ex.Message));
+                return "";
+            }
+        }
+
+
+        private void Parse(string html,string current)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+(html|htm|aspx|jsp)[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                if(strRef.StartsWith("//"))
+                {
+                    strRef = "https:" + strRef;
+                }
+                if (strRef.StartsWith("/"))
+                {
+                    strRef = strRef.Substring(1);
+                }
+
+
+                if (!strRef.StartsWith("https://") && !strRef.StartsWith("http://"))
+                {
+                    //去掉子地址开头的"/"，连接成完整地址
+                    strRef = current.Substring(0, current.IndexOf(".com") + 4) + "/" + strRef;
+
+                }
+                if (urls[strRef] == null)
+                {
+                    urls[strRef] = false;
+                    UrlGetList.Add(new Url(strRef));
+                }
+            }
+        }
+    }
+
+    public class Url
+    {
+        string urlMessage;
+        public Url(string MessageIn)
+        {
+            urlMessage = MessageIn;
+        }
+        public Url()
+        {
+            urlMessage = "";
+        }
+    }
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrawlerForm
+{
+    class Crawler
+    {
+        public event Action<Crawler> CrawlerStopped;
+        public event Action<Crawler, string, string> PageDownloaded;
+        //待下载队列
+        Queue<string> pending = new Queue<string>();
+        //已下载网页
+        public Dictionary<string, bool> DownloadedPages { get; } = new Dictionary<string, bool>();
+        //URL检测表达式，用于在HTML文本中查找URL
+        public static readonly string UrlDetectRegex = @"(href|HREF)[]*=[]*[""'](?<url>[^""'#>]+)[""']";
+        //URL解析表达式
+        public static readonly string urlParseRegex = @"^(?<site>(?<protocal>https?)://(?<host>[\w.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#?]*)";
+        //主机过滤规则
+        public string HostFilter { get; set; }
+        //文件过滤规则
+        public string FileFilter { get; set; }
+        //最大下载数量
+        public int MaxPage { get; set; }
+        //起始网址
+        public string StartURL { get; set; }
+        //网页编码
+        public Encoding HtmlEncoding { get; set; }
+
+        public Crawler()
+        {
+            MaxPage = 100;
+            HtmlEncoding = Encoding.UTF8;
+        }
+
+        public void Start()
+        {
+            DownloadedPages.Clear();
+            pending.Clear();
+            pending.Enqueue(StartURL);
+
+            while (DownloadedPages.Count < MaxPage && pending.Count > 0)
+            {
+                string url = pending.Dequeue();
+                try
+                {
+                    string html = DownLoad(url); // 下载
+                    DownloadedPages[url] = true;
+                    PageDownloaded(this, url, "success");
+                    Parse(html, url);//解析,并加入新的链接
+                }
+                catch (Exception ex)
+                {
+                    PageDownloaded(this, url, "  Error:" + ex.Message);
+                }
+            }
+            CrawlerStopped(this);
+        }
+
+        private string DownLoad(string url)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string html = webClient.DownloadString(url);
+            string fileName = DownloadedPages.Count.ToString();
+            File.WriteAllText(fileName, html, Encoding.UTF8);
+            return html;
+        }
+
+        private void Parse(string html, string pageUrl)
+        {
+            var matches = new Regex(UrlDetectRegex).Matches(html);
+            foreach (Match match in matches)
+            {
+                string linkUrl = match.Groups["url"].Value;
+                if (linkUrl == null || linkUrl == "" || linkUrl.StartsWith("javascript:")) continue;
+
+                linkUrl = FixUrl(linkUrl, pageUrl);//转绝对路径
+                                                   //解析出host和file两个部分，进行过滤
+                Match linkUrlMatch = Regex.Match(linkUrl, urlParseRegex);
+                string host = linkUrlMatch.Groups["host"].Value;
+                string file = linkUrlMatch.Groups["file"].Value;
+                if (Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, FileFilter)
+                  && !DownloadedPages.ContainsKey(linkUrl))
+                {
+                    pending.Enqueue(linkUrl);
+                }
+            }
+        }
+
+        //将非完整路径转为完整路径
+        static private string FixUrl(string url, string pageUrl)
+        {
+            if (url.Contains("://"))
+            { //完整路径
+                return url;
+            }
+            if (url.StartsWith("//"))
+            {
+                Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+                string protocal = urlMatch.Groups["protocal"].Value;
+                return protocal + ":" + url;
+            }
+            if (url.StartsWith("/"))
+            {
+                Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+
+            if (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = pageUrl.LastIndexOf('/');
+                return FixUrl(url, pageUrl.Substring(0, idx));
+            }
+
+            if (url.StartsWith("./"))
+            {
+                return FixUrl(url.Substring(2), pageUrl);
+            }
+            //非上述开头的相对路径
+            int end = pageUrl.LastIndexOf("/");
+            return pageUrl.Substring(0, end) + "/" + url;
+        }
+
+    }
+}   
+
+class SimpleCrawler
+{
+        static void Main(string[] args)
+        {
+
+        }
+}     
